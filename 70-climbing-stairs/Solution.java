cc8bbffc88530babb30de5f51e9108cfd3b127f3
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-03-07 01:56:50 +0500

    Runtime: 0 ms, faster than 100.00% , Memory Usage: 40.6 MB, less than 39.02%

---
+++ 70-climbing-stairs/Solution.java
@@ -0,0 +1,16 @@
+// basically , its a fabionacci series
+
+class Solution {
+    public int climbStairs(int n) {
+        int one = 1;
+        int two = 1;
+        for(int i = 0; i < n - 1 ; i++)
+        {
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+        
+        return one;
+    }
+}
