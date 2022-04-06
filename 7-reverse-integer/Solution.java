9b9f3fd797c25291dbd722574b693dc0196c7073
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-04-06 19:59:30 +0500

    Runtime: 1 ms, faster than 98.87% , Memory Usage: 41.8 MB, less than 25.31%

---
+++ 7-reverse-integer/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        while(x != 0){
+            rev = (rev * 10) + x % 10;
+            if(rev > Integer.MAX_VALUE){
+                return 0;
+            }
+            if(rev < Integer.MIN_VALUE){
+                return 0;
+            }
+            x = x / 10;
+        }
+        return (int)rev;
+    }
+}
