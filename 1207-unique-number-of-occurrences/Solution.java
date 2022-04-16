ffc9abf7684b0e6db2682e4027f2945bd0302a56
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-04-16 23:51:06 +0500

    Runtime: 2 ms, faster than 82.34% , Memory Usage: 40.3 MB, less than 88.51%

---
+++ 1207-unique-number-of-occurrences/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        
+        HashMap<Integer , Integer> map = new HashMap<Integer , Integer>();
+        
+        for(int i = 0 ; i < arr.length ; i++){
+            
+            if(map.containsKey(arr[i])){
+                map.put( arr[i] , map.get(arr[i]) + 1);
+            } else{
+                map.put(arr[i] , 1);
+            }
+            
+        }
+        
+        Set<Integer> set = new HashSet<>(map.values());
+        
+        return map.size() == set.size();
+    }
+}
