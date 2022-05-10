9a744a5f2fef2aeb0c66ca8942cba782d04fc05a
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-05-10 23:56:06 +0500

    Runtime: 1 ms, faster than 90.52% , Memory Usage: 42.4 MB, less than 28.87%

---
+++ 1662-check-if-two-string-arrays-are-equivalent/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        
+        StringBuilder w1 = new StringBuilder();
+        StringBuilder w2 = new StringBuilder();
+        
+        for(int i = 0 ; i < word1.length ; i++){
+            
+            w1.append(word1[i]);
+            
+        }
+        
+        for(int i = 0 ; i < word2.length ; i++){
+            
+            w2.append(word2[i]);
+            
+        }
+        
+        return w1.toString().equals(w2.toString());
+        
+    }
+}
