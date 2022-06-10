6f508652a9b1bc95f029897d04a7f258c915a503
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-06-10 21:49:01 +0500

    Runtime: 88 ms, faster than 78.71% , Memory Usage: 149 MB, less than 37.01%

---
+++ 2260-minimum-consecutive-cards-to-pick-up/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumCardPickup(int[] cards) {
+        
+        HashMap<Integer , Integer> map = new HashMap<Integer , Integer>();
+        int min = Integer.MAX_VALUE;
+        for(int i = 0 ; i < cards.length ; i++){
+            
+            if(map.containsKey(cards[i]))
+                min = Math.min(min , i - map.get(cards[i]) + 1);
+            
+            map.put(cards[i] , i);
+            
+        }
+        
+        return min == Integer.MAX_VALUE ? - 1 : min;
+    }
+}
