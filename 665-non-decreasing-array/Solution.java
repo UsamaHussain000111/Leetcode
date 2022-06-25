910e2dc4528c39b9c06e482cc05e20b25fea8d21
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-06-25 22:34:35 +0500

    Runtime: 2 ms, faster than 30.74% , Memory Usage: 54 MB, less than 41.22%

--- 665-non-decreasing-array/Solution.java
+++ 665-non-decreasing-array/Solution.java
@@ -1,4 +1,8 @@
-// This problem is like a greedy problem. When you find nums[i-1] > nums[i] for some i, you will prefer to change nums[i-1]'s value, since a larger nums[i] will give you more risks that you get inversion errors after position i. But, if you also find nums[i-2] > nums[i], then you have to change nums[i]'s value instead, or else you need to change both of nums[i-2]'s and nums[i-1]'s values.
+// This problem is like a greedy problem. When you find nums[i-1] > nums[i] for some i, 
+// you will prefer to change nums[i-1]'s value, since a larger nums[i] will give you more 
+// risks that you get inversion errors after position i. But, if you also find nums[i-2] > nums[i],
+// then you have to change nums[i]'s value instead, 
+// or else you need to change both of nums[i-2]'s and nums[i-1]'s values.
 
 class Solution {
     
@@ -9,7 +13,7 @@ class Solution {
         for(int i = 1; i < nums.length ; i++){
             
             if(nums[i-1] > nums[i]){
-                
+
                 cnt++;
                
                 if( i - 2 < 0 || nums[ i - 2 ] <= nums[i] ) nums[ i - 1 ] = nums[i]; 
