caed530c297f3d7e442c3fd3c97043dabc2af768
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-08-22 19:59:48 +0500

    Runtime: 1 ms, faster than 100.00%, Memory Usage: 41.7 MB, less than 25.85%

--- 342-power-of-four/Solution.java
+++ 342-power-of-four/Solution.java
@@ -22,8 +22,9 @@ class Solution {
         return n>0 && (n & (n-1)) == 0 && (n-1)%3==0;
     }
 }
-Explanation :
 
-- n>0 is there to check if the number is positive, since negative numbers are not power of 4.
-- (n & (n-1)) == 0 is there to check if the binary representation of the number has only one 1 in it.
-- (n-1)%3==0 is to check 4 to the power x minus 1 is divisible by 3. This is mathematically true for all powers of 4.
+// Explanation:
+
+// - n>0 is there to check if the number is positive, since negative numbers are not power of 4.
+// - (n & (n-1)) == 0 is there to check if the binary representation of the number has only one 1 in it.
+// - (n-1)%3==0 is to check 4 to the power x minus 1 is divisible by 3. This is mathematically true for all powers of 4.
