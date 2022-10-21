fc9c6927ad3d801f9f4857235e3c365738ca2158
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-10-21 15:49:41 +0500

    Runtime: 18 ms, faster than 97.16%, Memory Usage: 50.6 MB, less than 93.10%

--- 219-contains-duplicate-II/Solution.java
+++ 219-contains-duplicate-II/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        
+        // create a hashmap to keep track of nums[i] index
         HashMap<Integer , Integer> map = new HashMap<Integer , Integer>();
-        
+        // loop over the array
         for(int i = 0 ; i < nums.length ; i++){
-            
+            // if map contains the key(nums[i]) && if i - the index <= k
             if(map.containsKey(nums[i]) && (i - map.get(nums[i]) <= k ) ) {
-                return true;
+                return true; //return true
             }
-            map.put(nums[i] , i);
+            map.put(nums[i] , i); // keep putting the nums[i] and their last index
         }
         
-        return false;
+        return false; // otherwise the answer will always be false
     }
 }
