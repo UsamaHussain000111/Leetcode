13c92b9b3e7aa8d9e942d84375fbbc457771fd0d
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-12-12 15:23:08 +0500

    Runtime 0 ms Beats 100% Memory 40.5 MB Beats 67.53%

--- 70-climbing-stairs/Solution.java
+++ 70-climbing-stairs/Solution.java
@@ -1,9 +1,15 @@
-// basically , its a fabionacci series
+// basically , its a fibonacci series
+// why its a fibonacci series? to find out refer to the following videos:
+// https://www.youtube.com/watch?v=Y0lT9Fck7qI (neetcode)
+// https://www.youtube.com/watch?v=uHAToNgAPaM (kevin) (using dp)
+//  https://www.youtube.com/watch?v=5o-kdjv7FD0 (cs dojo) (using dp)
 
 class Solution {
     public int climbStairs(int n) {
+
         int one = 1;
         int two = 1;
+
         for(int i = 0; i < n - 1 ; i++)
         {
             int temp = one;
