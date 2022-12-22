eca45de213ae124efe94a459b047e99cd5fa7e41
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-12-23 01:07:46 +0500

    Runtime 8 ms Beats 61.20% Memory 47.2 MB Beats 81.49%

--- 169-majority-element/Solution.java
+++ 169-majority-element/Solution.java
@@ -1,30 +1,41 @@
-// My solution using hash map
-// Runtime: 9 ms, faster than 34.33% :(
-// Memory Usage: 47.1 MB, less than 82.71% :/
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for(int num : nums)
+            map.put(num , map.getOrDefault(num , 0) + 1);
+
+        int max = 0;
+
+        for(int i : map.keySet()){
+
+            int curr = map.get(i);
+
+            if(curr > (nums.length / 2))
+                max = i;
+        }
+
+        return max;
+    }
+}
+
+// with O(1) space
 
 class Solution {
     public int majorityElement(int[] nums) {
-        
-        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length ; i++)
-        {
-            if(!counts.containsKey(nums[i]))
-            {
-                counts.put(nums[i] , 1);
-            }
-            else
-            {
-                counts.put(nums[i] ,  counts.get(nums[i]) + 1);
-            }
-        }
-        
-        Map.Entry<Integer, Integer> majorityEntry = null;
-        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-            if(majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
-                majorityEntry = entry;
-            }
-        }
-
-        return majorityEntry.getKey();
+        int count = 0;
+        int current = nums[0];
+        for(int i = 0; i< nums.length;i++){
+			if(count <= 0){
+				current = nums[i]; 
+				count = 0;
+			}
+			if(current == nums[i])
+				count++;
+			if(current != nums[i])
+				count--;
+        }  
+    return current;
     }
 }
