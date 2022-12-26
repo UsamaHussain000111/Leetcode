623a619befc006f6f6d6587ff2faff6309247ee8
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-12-26 15:50:37 +0500

    Runtime 2 ms Beats 92.42% Memory 43 MB Beats 78.17%

---
+++ 55-jump-game/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max = nums[0];
+
+        for(int i = 0; i <= max ; i++){
+
+            max = Math.max(max , i + nums[i]);
+            if(i >= nums.length - 1)
+                return true;
+        }
+        return false;
+    }
+}
