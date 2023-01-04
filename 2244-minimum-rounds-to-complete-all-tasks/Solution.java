541e869f319b8ec9b5cfe713e53e883d82fd8ff7
UsamaHussain000111 <usamahussaincs@gmail.com>
2023-01-04 19:51:39 +0500

    Runtime 33 ms Beats 90.96% Memory 56.1 MB Beats 87.83%

---
+++ 2244-minimum-rounds-to-complete-all-tasks/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        
+        HashMap<Integer , Integer> map = new HashMap<Integer , Integer>();
+        
+        for(int i = 0 ; i < tasks.length ; i++ )
+            map.put( tasks[i] , map.getOrDefault(tasks[i] , 0) + 1 );
+
+        int count = 0;
+
+        for(int freq: map.values()){
+            
+            if(freq == 1)
+                return -1;
+
+                count += (freq + 2) / 3;
+        }
+
+        return count;
+    }
+}
