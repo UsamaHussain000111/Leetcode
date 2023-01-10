4eff0ffbbc4c14c045f86fc2a401883f335f62be
UsamaHussain000111 <usamahussaincs@gmail.com>
2023-01-10 18:26:19 +0500

    Runtime 2 ms Beats 88.8% Memory 42.7 MB Beats 69.31%

--- 1-two-sum/Solution.java
+++ 1-two-sum/Solution.java
@@ -3,16 +3,17 @@ import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        HashMap<Integer , Integer> hash_map = new HashMap<Integer , Integer>(); 
+        HashMap<Integer , Integer> map = new HashMap<Integer , Integer>(); 
+
         for(int i = 0 ; i < nums.length; i++)
         {   
             int complement = target - nums[i]; 
-            if(hash_map.containsKey(complement))
+            if(map.containsKey(complement))
             {
-                return new int[] { hash_map.get(complement) , i };
+                return new int[] { map.get(complement) , i };
             }
             
-            hash_map.put(nums[i] , i);
+            map.put(nums[i] , i);
         }
         
         throw new IllegalArgumentException("no match found");
