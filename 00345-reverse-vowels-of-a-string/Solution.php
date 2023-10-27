e8bf77eb2c4efefdbecb5de23cd1517ba1325612
UsamaHussain000111 <usamahussaincs@gmail.com>
2023-10-27 16:28:31 +0500

    34ms Beats 21.02% of users with PHP, 27.55MB Beats 26.12% of users with PHP

--- 00345-reverse-vowels-of-a-string/Solution.php
+++ 00345-reverse-vowels-of-a-string/Solution.php
@@ -1,35 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * @param array<int> $arr
+ * @param int $i
+ * @param int $j
+ * @return void
+ */
+function swap(array &$arr, int $i, int $j): void {
+    $temp = $arr[$i];
+    $arr[$i] = $arr[$j];
+    $arr[$j] = $temp;
+}
+
+/**
+ * @param string $C
+ * @return bool
+ */
+function isVowel(string $c): bool {
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    return in_array(strtolower($c), $vowels);
+}
+
 class Solution {
 
-    function swap(array &$arr, int $i, int $j): void {
-        $temp = $arr[$i];
-        $arr[$i] = $arr[$j];
-        $arr[$j] = $temp;
-    }
-
-    function isVowel(string $c): bool {
-        $vowels = ['a', 'e', 'i', 'o', 'u'];
-        return in_array(strtolower($c), $vowels);
-    }
-
-
     /**
-     * @param String $s
-     * @return String
+     * @param string $s
+     * @return string
      */
-    function reverseVowels($s) {
+    function reverseVowels(string $s): string {
         $l = 0;
         $r = strlen($s) - 1;
         $arr = str_split($s);
 
         while($l < $r) {
 
-            if($this->isVowel($arr[$l]) && $this->isVowel($arr[$r])) {
-                $this->swap($arr, $l, $r);
+            if(isVowel($arr[$l]) && isVowel($arr[$r])) {
+                swap($arr, $l, $r);
                 $l++;
                 $r--;
-            }else if($this->isVowel($arr[$l])) {
+            }else if(isVowel($arr[$l])) {
                 $r--;
             }else {
                 $l++;
@@ -39,3 +50,5 @@ class Solution {
         return implode($arr);
     }
 }
+
+// 34ms Beats 21.02% of users with PHP, 27.55MB Beats 26.12% of users with PHP
